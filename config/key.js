418c999ea425c09fb76c22e0a90d2df2a35281cb
Houda saira <houdasaira@gmail.com>
2022-03-23 11:36:48 +0100

    first commit

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://houda20:taylorLAUTNER@houdadb.falyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/key').mongoURI ;
+
+mongoose
+ .connect(db)
+ .then( ()=> console.log('MongoDB Connected...') )
+ .catch(err=> console.log(err)) ;
+
+ const port = process.env.PORT || 5000 ;
+
+ app.use('/api/item' , items) ; 
+
+ app.listen(port , ()=>console.log(`Server started on port ${port}`)) ;
+
+ 
+ 
+
+
+
